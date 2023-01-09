37bc219c0b6a4a93d30d9f5fd4669b67c47ce369
Justin-Lavoie16 <Justin-Lavoie16@users.noreply.github.com>
2023-01-09 18:58:13 +0000

    Prettified Code!

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ if (navigator.serviceWorker) {
   })
 }
 
-"use strict"
+;("use strict")
 
 const ROCK = 1
 const PAPER = 2
@@ -14,109 +14,104 @@ function updateSliderValue(valueFromSlider) {
   document.getElementById("slider-value").innerHTML = valueFromSlider
 }
 
-
-
 function myButtonClicked() {
-
   var random = Math.floor(Math.random() * 3) + 1
   var paperSelected = document.getElementById("paper").checked
   var rockSelected = document.getElementById("rock").checked
   var scissorSelected = document.getElementById("scissor").checked
-  var message = "";
+  var message = ""
 
-  
-    if (localStorage.CookieCountGoUp) {
-    localStorage.CookieCountGoUp = Number(localStorage.CookieCountGoUp) + 1;
+  if (localStorage.CookieCountGoUp) {
+    localStorage.CookieCountGoUp = Number(localStorage.CookieCountGoUp) + 1
   } else {
-    localStorage.CookieCountGoUp = +1;
+    localStorage.CookieCountGoUp = +1
   }
   document.getElementById("answer").innerHTML =
-    "You have played against the bot " + localStorage.CookieCountGoUp + " times already";
+    "You have played against the bot " +
+    localStorage.CookieCountGoUp +
+    " times already"
 
-  
   if (random == ROCK) {
     if (paperSelected) {
-      message = "The computer choose ✊ rock! You win!";
-    localStorage.wins = Number(localStorage.wins) + 1;
+      message = "The computer choose ✊ rock! You win!"
+      localStorage.wins = Number(localStorage.wins) + 1
     } else {
-    localStorage.wins = +1;
-        
+      localStorage.wins = +1
     }
-     if (random == ROCK) { 
+    if (random == ROCK) {
+      if (scissorSelected) {
+        message = "The computer choose ✊ rock! You lose!"
+        localStorage.lost = Number(localStorage.lost) + 1
+      } else {
+        localStorage.lost = +1
+      }
+    }
+
+    if (random == ROCK) {
+      if (rockSelected) {
+        message = "The computer choose ✊ rock! It's a tie"
+        localStorage.ties = Number(localStorage.ties) + 1
+      } else {
+        localStorage.ties = +1
+      }
+    }
+  }
+  if (random == PAPER) {
     if (scissorSelected) {
-      message = "The computer choose ✊ rock! You lose!";
-            localStorage.lost = Number(localStorage.lost) + 1;
+      message = "The computer choose ✋ paper! You win!"
+      localStorage.wins = Number(localStorage.wins) + 1
     } else {
-    localStorage.lost = +1;
+      localStorage.wins = +1
     }
-     }
-
-     if (random == ROCK) { 
+  }
+  if (random == PAPER) {
     if (rockSelected) {
-      message = "The computer choose ✊ rock! It's a tie";
-            localStorage.ties = Number(localStorage.ties) + 1;
-          } else {
-    localStorage.ties = +1;
-    }
-     }
-  
-  } if (random == PAPER) {
-        if (scissorSelected) {
-      message = "The computer choose ✋ paper! You win!";
-localStorage.wins = Number(localStorage.wins) + 1;
+      message = "The computer choose ✋ Paper! You lose!"
+      localStorage.lost = Number(localStorage.lost) + 1
     } else {
-    localStorage.wins = +1;
-        }
+      localStorage.lost = +1
+    }
   }
-  if (random == PAPER) { 
+  if (random == PAPER) {
+    if (paperSelected) {
+      message = "The computer choose ✋ Paper! It's a tie"
+      localStorage.ties = Number(localStorage.ties) + 1
+    } else {
+      localStorage.ties = +1
+    }
+  }
+  if (random == SCISSORS) {
     if (rockSelected) {
-      message = "The computer choose ✋ Paper! You lose!";
-            localStorage.lost = Number(localStorage.lost) + 1;
-          } else {
-    localStorage.lost = +1;
-    }
-  }
-   if (random == PAPER) { 
-    if (paperSelected) {
-      message = "The computer choose ✋ Paper! It's a tie";
-            localStorage.ties = Number(localStorage.ties) + 1;
-          } else {
-    localStorage.ties = +1;
-    }
-     }
-   if (random == SCISSORS) {
-        if (rockSelected) {
-      message = "The computer choose ✌️ Scissor! You win!";
-localStorage.wins = Number(localStorage.wins) + 1;
+      message = "The computer choose ✌️ Scissor! You win!"
+      localStorage.wins = Number(localStorage.wins) + 1
     } else {
-    localStorage.wins = +1;
-        }
-}
-if (random == SCISSORS) { 
-    if (paperSelected) {
-      message = "The computer choose ✌️ Scissor! You lose!";
-            localStorage.lost = Number(localStorage.lost) + 1;
-          } else {
-    localStorage.lost = +1;
+      localStorage.wins = +1
     }
   }
-  if (random == SCISSORS) { 
+  if (random == SCISSORS) {
+    if (paperSelected) {
+      message = "The computer choose ✌️ Scissor! You lose!"
+      localStorage.lost = Number(localStorage.lost) + 1
+    } else {
+      localStorage.lost = +1
+    }
+  }
+  if (random == SCISSORS) {
     if (scissorSelected) {
-      message = "The computer choose ✌️ Scissor! It's a tie";
-            localStorage.ties = Number(localStorage.ties) + 1;
-          } else {
-    localStorage.ties = +1;
+      message = "The computer choose ✌️ Scissor! It's a tie"
+      localStorage.ties = Number(localStorage.ties) + 1
+    } else {
+      localStorage.ties = +1
     }
-     }
-  document.getElementById("hello-world").innerHTML = message;
-  
-  document.getElementById("win").innerHTML = "you have won " + localStorage.wins + " times"
+  }
+  document.getElementById("hello-world").innerHTML = message
 
-    document.getElementById("lose").innerHTML = "you have lost " + localStorage.lost + " times"
+  document.getElementById("win").innerHTML =
+    "you have won " + localStorage.wins + " times"
 
-      document.getElementById("tie").innerHTML = "you have tied " + localStorage.ties + " times"
+  document.getElementById("lose").innerHTML =
+    "you have lost " + localStorage.lost + " times"
 
+  document.getElementById("tie").innerHTML =
+    "you have tied " + localStorage.ties + " times"
 }
-
-
-
